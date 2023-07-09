0f50633a704a7e5fbacfbf0e6d492e88ee34fd17
guskumite <gmolanoruiz2000@gmail.com>
2023-07-08 22:59:52 -0500

    theGroups report version 1.0.0

---
+++ main.js
@@ -0,0 +1,517 @@
+const data = [
+	{
+		id: 1,
+		title: "Grupo 1",
+		color: "#7895CB",
+		subGroups: [
+			{
+				id: 1,
+				title: "Grupo 1 - A",
+				participants: [
+					{
+						id: 1,
+						name: "Andres",
+						email: "andres@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Andre",
+						email: "andre@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Ana",
+						email: "ana@academlo.com",
+					},
+					{
+						id: 4,
+						name: "Alicia",
+						email: "alicia@academlo.com",
+					},
+					{
+						id: 5,
+						name: "Anderson",
+						email: "anderson@academlo.com",
+					},
+				],
+			},
+			{
+				id: 2,
+				title: "Grupo 1 - B",
+				participants: [
+					{
+						id: 1,
+						name: "Brayan",
+						email: "brayan@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Bruno",
+						email: "bruno@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Bernardo",
+						email: "bernardo@academlo.com",
+					},
+				],
+			},
+			{
+				id: 3,
+				title: "Grupo 1 - C",
+				participants: [
+					{
+						id: 1,
+						name: "Cameron",
+						email: "cameron@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Camilo",
+						email: "camilo@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Camila",
+						email: "camila@academlo.com",
+					},
+					{
+						id: 4,
+						name: "Caleb",
+						email: "caleb@academlo.com",
+					},
+				],
+			},
+			{
+				id: 3,
+				title: "Grupo 1 - D",
+				participants: [
+					{
+						id: 1,
+						name: "Dante",
+						email: "dante@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Damián",
+						email: "damián@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Darío",
+						email: "darío@academlo.com",
+					},
+					{
+						id: 4,
+						name: "David",
+						email: "david@academlo.com",
+					},
+					{
+						id: 5,
+						name: "Danna",
+						email: "danna@academlo.com",
+					},
+				],
+			},
+		],
+	},
+	{
+		id: 2,
+		title: "Grupo 2",
+		color: "#FF9B9B",
+		subGroups: [
+			{
+				id: 1,
+				title: "Grupo 2 - E",
+				participants: [
+					{
+						id: 1,
+						name: "Elías",
+						email: "Elías@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Eugenio",
+						email: "Eugenio@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Enrique",
+						email: "Enrique@academlo.com",
+					},
+				],
+			},
+			{
+				id: 2,
+				title: "Grupo 2 - F",
+				participants: [
+					{
+						id: 1,
+						name: "Fabrizio",
+						email: "Fabrizio@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Federico",
+						email: "Federico@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Fabián",
+						email: "Fabián@academlo.com",
+					},
+				],
+			},
+			{
+				id: 3,
+				title: "Grupo 1 - G",
+				participants: [
+					{
+						id: 1,
+						name: "Gael",
+						email: "Gael@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Gabriel",
+						email: "Gabriel@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Gaspar",
+						email: "Gaspar@academlo.com",
+					},
+					{
+						id: 4,
+						name: "Gastón",
+						email: "Gastón@academlo.com",
+					},
+				],
+			},
+			{
+				id: 3,
+				title: "Grupo 1 - H",
+				participants: [
+					{
+						id: 1,
+						name: "Homero",
+						email: "Homero@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Héctor",
+						email: "Héctor@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Hércules",
+						email: "Hércules@academlo.com",
+					},
+					{
+						id: 4,
+						name: "Heraclio",
+						email: "Heraclio@academlo.com",
+					},
+					{
+						id: 5,
+						name: "Heracles",
+						email: "Heracles@academlo.com",
+					},
+				],
+			},
+		],
+	},
+	{
+		id: 3,
+		title: "Grupo 3",
+		color: "#C3EDC0",
+		subGroups: [
+			{
+				id: 1,
+				title: "Grupo 3 - I",
+				participants: [
+					{
+						id: 1,
+						name: "Ida",
+						email: "Ida@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Iola",
+						email: "Iola@academlo.com",
+					},
+				],
+			},
+			{
+				id: 2,
+				title: "Grupo 3 - J",
+				participants: [
+					{
+						id: 1,
+						name: "Jaylin",
+						email: "Jaylin@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Jeanette",
+						email: "Jeanette@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Janessa",
+						email: "Janessa@academlo.com",
+					},
+					{
+						id: 4,
+						name: "Jessica",
+						email: "Jessica@academlo.com",
+					},
+					{
+						id: 5,
+						name: "Jacqueline",
+						email: "Jacqueline@academlo.com",
+					},
+					{
+						id: 6,
+						name: "Jade",
+						email: "Jade@academlo.com",
+					},
+				],
+			},
+			{
+				id: 3,
+				title: "Grupo 3 - K",
+				participants: [
+					{
+						id: 1,
+						name: "Katherine",
+						email: "Katherine@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Karina",
+						email: "Karina@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Kenia",
+						email: "Kenia@academlo.com",
+					},
+					{
+						id: 4,
+						name: "Kiara",
+						email: "Kiara@academlo.com",
+					},
+				],
+			},
+		],
+	},
+	{
+		id: 3,
+		title: "Grupo aldea de la hoja",
+		color: "#FFA500",
+		subGroups: [
+			{
+				id: 1,
+				title: "Grupo Kakashi",
+				participants: [
+					{
+						id: 1,
+						name: "Naruto Uzumaki",
+						email: "NarutoUzumaki@konoha.com",
+					},
+					{
+						id: 2,
+						name: "Sakura Haruno",
+						email: "SakuraHaruno@konoha.com",
+					},
+					{
+						id: 3,
+						name: "Sasuke Uchiha",
+						email: "SasukeUchiha@konoha.com",
+					},
+				],
+			},
+			{
+				id: 2,
+				title: "Grupo Kurenai",
+				participants: [
+					{
+						id: 1,
+						name: "Kiba Inuzuka",
+						email: "KibaInuzuka@konoha.com",
+					},
+					{
+						id: 2,
+						name: "Hinata Hyūga",
+						email: "HinataHyūga@konoha.com",
+					},
+					{
+						id: 3,
+						name: " Shino Aburame",
+						email: " ShinoAburame@konoha.com",
+					},
+				],
+			},
+			{
+				id: 3,
+				title: "Grupo Asuma",
+				participants: [
+					{
+						id: 1,
+						name: " Ino Yamanaka",
+						email: " InoYamanaka@academlo.com",
+					},
+					{
+						id: 2,
+						name: " Shikamaru Nara",
+						email: " ShikamaruNara@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Chōji Akimichi",
+						email: "ChōjiAkimichi@academlo.com",
+					},
+				],
+			},
+			{
+				id: 3,
+				title: "Grupo Guy",
+				participants: [
+					{
+						id: 1,
+						name: "Tenten",
+						email: "Tenten@academlo.com",
+					},
+					{
+						id: 2,
+						name: "Neji Hyūga",
+						email: "NejiHyūga@academlo.com",
+					},
+					{
+						id: 3,
+						name: "Rock Lee",
+						email: "RockLee@academlo.com",
+					},
+				],
+			},
+		],
+	},
+	{
+		id: 4,
+		title: "Grupos de Rock",
+		color: "#FFC0C0",
+		subGroups: [
+			{
+				id: 1,
+				title: "Grupo Joan Jett and the Blackhearts",
+				participants: [
+					{
+						id: 1,
+						name: "Joan Jett",
+						email: "I don't know",
+					},
+					{
+						id: 2,
+						name: "Ricky Byrd",
+						email: "Who cares?",
+					},
+					{
+						id: 3,
+						name: "John Doe",
+						email: "fakeemail@themail.net",
+					},
+{
+						id: 4,
+						name: "Gary Ryan",
+						email: "ihatemyself@forloving.you",
+					},
+				],
+			},
+			{
+				id: 2,
+				title: "Wagakki Band",
+				participants: [
+					{
+						id: 1,
+						name: "Yūko Suzuhana (鈴華ゆう子?)",
+						email: "vocalista",
+					},
+					{
+						id: 2,
+						name: "Kiyoshi Ibukuro (いぶくろ聖志?)",
+						email: "koto",
+					},
+					{
+						id: 3,
+						name: "Daisuke Kaminaga (神永大輔?)",
+						email: "Shakuhachi",
+					},
+					{
+						id: 4,
+						name: "Beni Ninagawa (蜷川べに?)",
+						email: "Tsugaru Shamisen",
+					},
+					{
+						id: 5,
+						name: "Kurona (黒流?)",
+						email: "Wadaiko",
+					},
+					{
+						id: 6,
+						name: "Machiya (町屋?)",
+						email: "Guitarra Eléctrica",
+					},
+					{
+						id: 7,
+						name: "Asa (亜沙?)",
+						email: "Bajo eléctrico",
+					},
+					{
+						id: 8,
+						name: "Wasabi (山葵?)",
+						email: "Batería",
+					},
+
+				],
+			},]}
+];
+
+function thePrinter(data) {
+	 let groupHTML = document.querySelector('.group');
+	 let html = "";
+	 
+	 for (grupo of data) {
+		html += `
+		<h2 id="${grupo.color}"> ${grupo.title}</h2>		
+		`;
+		html += `<div class="subgroups"> <div class="subgroup">
+		`;
+		for (subgrupo of grupo.subGroups) {
+			html += `
+			<h2 id="${subgrupo.title}">${subgrupo.title}</h2>
+			`;
+			html += `<div class="participants">
+			`;
+			for (participante of subgrupo.participants) {
+				html += `
+				<div class="participant">
+				<h2>${participante.name} <br> ${participante.email} </h2>
+				</div>
+				`;
+			}
+			html += `</div>`
+		}
+	 }
+	 groupHTML.innerHTML = html;
+
+	 for (i = 0; i <= data.length - 1; i++) {	
+         document.getElementById(`${data[i].color}`).style.color = `${data[i].color}`;
+		 for (j = 0; j <= data[i].subGroups.length - 1; j++) {
+			document.getElementById(`${data[i].subGroups[j].title}`).style.color = `${data[i].color}`;
+		 }
+	 }
+	 
+}
+
+thePrinter(data);
